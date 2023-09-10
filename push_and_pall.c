5ac19e4720b914ca266f3b9ddc2f242ad4019881
neisnei <130294790+neisnei@users.noreply.github.com>
2023-09-10 02:20:59 +0200

    Update push_and_pall.c

--- push_and_pall.c
+++ push_and_pall.c
@@ -34,23 +34,3 @@ if (*stack != NULL)
 *stack = new_node;
 
 }
-
-/**
- * pall - prints all the values on the stack.
- * @stack: double pointer to the head of the stack.
- * @line_number: number count in the command line of Monty file.
- */
-
-void pall(stack_t **stack, unsigned int line_number)
-{
-    stack_t *current;
-
-    (void)line_number;
-
-    current = *stack;
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
