1dd60c5d02777d38e901f0511e3ffae87e110d42
neisnei <130294790+neisnei@users.noreply.github.com>
2023-09-10 02:20:08 +0200

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pall - prints all the values on the stack.
+ * @stack: double pointer to the head of the stack.
+ * @line_number: number count in the command line of Monty file.
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current;
+
+    (void)line_number;
+
+    current = *stack;
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
