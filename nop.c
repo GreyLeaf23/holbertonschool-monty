8f2f06d4e101436e623e1cc54ce6eb2f0b517dd4
neisnei <neischalyruidiaz@gmail.com>
2023-09-11 17:32:47 +0000

    Existing Files

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop - doesn't do anything.
+ * @stack: double pointer to the head of the stack.
+ * @line_number: number count in the command line for Monty file.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+    /* This function does nothing */
+}
---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * pint - prints the value at the top of the stack.
+ * @stack: pointer to the head of the stack.
+ * @line_number: number count in the command line for Monty file.
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack) == NULL)/*Error Check*/
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n); /*Print the value at the top*/
+}
